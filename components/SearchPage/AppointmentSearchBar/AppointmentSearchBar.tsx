ae81f80bafb2973b8cd94ea262c13dcdb990d13c
Frank Jia <jiafrank98@gmail.com>
2020-12-23 18:44:30 -0800

    Add components to search bar

--- components/SearchPage/AppointmentSearchBar/AppointmentSearchBar.tsx
+++ components/SearchPage/AppointmentSearchBar/AppointmentSearchBar.tsx
@@ -1,9 +1,41 @@
-import { Col, Input, Row, Space } from 'antd';
-import { useState } from 'react';
-import { BiCurrentLocation } from 'react-icons/bi';
+import { Button, Input, Row, Select, Space } from 'antd';
 import AppointmentDatePicker, {
   AppointmentDatePickerProps,
 } from '../AppointmentDatePicker/AppointmentDatePicker';
+import React, { PropsWithChildren, ReactNode } from 'react';
+import { FilterOutlined, SortAscendingOutlined } from '@ant-design/icons';
+import { BiCurrentLocation } from 'react-icons/bi';
+
+function LocationTypeSelect(): JSX.Element {
+  const items = ['Doctor', 'Dentist', 'Physiotherapist', 'Massage Therapist'];
+  return (
+    <Select style={{ width: '200px', textAlign: 'left' }} value={items[0]}>
+      {items.map((item) => {
+        return (
+          <Select.Option value={item} key={item}>
+            {item}
+          </Select.Option>
+        );
+      })}
+    </Select>
+  );
+}
+
+interface SearchItemWrapperProps {
+  title: string;
+}
+
+function SearchItemWrapper({
+  title,
+  children,
+}: PropsWithChildren<ReactNode> & SearchItemWrapperProps): JSX.Element {
+  return (
+    <div>
+      <div style={{ textAlign: 'left' }}>{title}</div>
+      {children}
+    </div>
+  );
+}
 
 /*
 Disabled input for current location
@@ -29,15 +61,20 @@ export default function AppointmentSearchBar({
   searchDateProps,
 }: AppointmentSearchBarProps): JSX.Element {
   return (
-    <>
-      <Row>
-        <Col>
-          <Space>
-            <LocationSearchInput />
-            <AppointmentDatePicker {...searchDateProps} />
-          </Space>
-        </Col>
-      </Row>
-    </>
+    <Row>
+      <Space align={'end'}>
+        <SearchItemWrapper title={'Location'}>
+          <LocationSearchInput />
+        </SearchItemWrapper>
+        <SearchItemWrapper title={'Date'}>
+          <AppointmentDatePicker {...searchDateProps} />
+        </SearchItemWrapper>
+        <SearchItemWrapper title={'Type'}>
+          <LocationTypeSelect />
+        </SearchItemWrapper>
+        <Button icon={<FilterOutlined />}>Filter</Button>
+        <Button icon={<SortAscendingOutlined />}>Sort</Button>
+      </Space>
+    </Row>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import styles from '../styles/Home.module.scss';
 import { Button, Layout } from 'antd';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { DateTime } from 'luxon';
+import AppointmentSearchBar from '../components/SearchPage/AppointmentSearchBar/AppointmentSearchBar';
+const { Footer, Content } = Layout;
 
 export default function Home() {
   const router = useRouter();
-  const { Footer, Content } = Layout;
 
-  const onSearchClicked = () => {
+  const [searchDate, setSearchDate] = useState<DateTime>(DateTime.local());
+
+  const onSearchClicked = (): void => {
     router.push('/search');
   };
 
@@ -17,6 +22,12 @@ export default function Home() {
           <div>
             <h1>Dr.Chrono Discover</h1>
             <h4>Find an available doctor near you.</h4>
+            <AppointmentSearchBar
+              searchDateProps={{
+                value: searchDate,
+                onChange: setSearchDate,
+              }}
+            />
             <Button type="primary" onClick={onSearchClicked}>
               Search Now
             </Button>
